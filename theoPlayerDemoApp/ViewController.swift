28cd8bcc2efc84b33a90d8f951ee6d2954a99de3
medabida <medabida2428@gmail.com>
2018-09-27 16:58:53 +0100

    Initial Commit

---
+++ theoPlayerDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theoPlayerDemoApp
+//
+//  Created by Med on 9/27/18.
+//  Copyright © 2018 Med Abida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
